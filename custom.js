ed06d5e1cf76d554d59bc4bb594a06eda3ab2cb2
lhudson19 <50920319+lhudson19@users.noreply.github.com>
2019-09-03 08:44:13 -0400

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,593 @@
+const data = {
+    "Now": [
+        {
+            name: 'Zika',
+            data: [{
+                name: 'Germany',
+                value: 2    
+            }, {
+                name: 'Croatia',
+                value: 4
+            }, {
+                name: 'Russia',
+                value: 5
+            }]
+        },
+        {
+            name: 'pneumonia',
+            data: [{
+                name: 'France',
+                value: 1
+            }, {
+                name: 'China',
+                value: 2
+            }, {
+                name: 'India',
+                value: 3
+            }]
+        },
+        {
+            name: 'Ebola',
+            data: [{
+                name: 'Germany',
+                value: 4
+            }, {
+                name: 'France',
+                value: 6
+            }, {
+                name: 'Chad',
+                value: 3
+            }, {
+                name: 'South Africa',
+                value: 5
+            }, {
+                name: 'Mali',
+                value: 5
+            }]
+        },
+        {
+            name: 'common cold',
+            data: [{
+                name: 'China',
+                value: 2
+            }, {
+                name: 'Iran',
+                value: 3
+            }, {
+                name: 'Spain',
+                value: 8
+            }]
+        },
+    ],
+    "1 week": [
+        {
+            name: 'Zika',
+            data: [{
+                name: 'Australia',
+                value: 20
+            }, {
+                name: 'Ukraine',
+                value: 14
+            }, {
+                name: 'Philippines',
+                value: 5
+            },{
+                name: 'Sudan',
+                value: 40
+            }, {
+                name: 'Chad',
+                value: 21
+            }, {
+                name: 'Canada',
+                value: 12
+            },{
+                name: 'Finland',
+                value: 30
+            }, {
+                name: 'Mali',
+                value: 10
+            }, {
+                name: 'Syria',
+                value: 21
+            }]
+        },
+        {
+            name: 'pneumonia',
+            data: [{
+                name: 'France',
+                value: 10
+            }, {
+                name: 'China',
+                value: 5
+            }, {
+                name: 'India',
+                value: 23
+            }]
+        },
+        {
+            name: 'Ebola',
+            data: [{
+                name: 'Burma',
+                value: 13
+            }, {
+                name: 'Malaysia',
+                value: 26
+            }, {
+                name: 'Chad',
+                value: 30
+            }, {
+                name: 'South Africa',
+                value: 12
+            }, {
+                name: 'Mali',
+                value: 38
+            }, {
+                name: 'Germany',
+                value: 12
+            }, {
+                name: 'Morocco',
+                value: 3
+            }]
+        },
+        {
+            name: 'common cold',
+            data: [{
+                name: 'China',
+                value: 2
+            }, {
+                name: 'Iran',
+                value: 3
+            }, {
+                name: 'Spain',
+                value: 8
+            }]
+        },
+    ],
+    "1 day": [
+        {
+            name: 'Ebola',
+            data: [{
+                name: 'Germany',
+                value: 14
+            }, {
+                name: 'Belarus',
+                value: 28
+            }, {
+                name: 'Russia',
+                value: 5
+            }, {
+                name: 'Moldova',
+                value: 12
+            }, {
+                name: 'Niger',
+                value: 11
+            }, {
+                name: 'Egypt',
+                value: 5
+            }]
+        },
+        {
+            name: 'pneumonia',
+            data: [{
+                name: 'Colombia',
+                value: 10
+            }, {
+                name: 'China',
+                value: 13
+            }, {
+                name: 'India',
+                value: 12
+            }, {
+                name: 'France',
+                value: 18
+            }, {
+                name: 'Algeria',
+                value: 20
+            }, {
+                name: 'Tunisia',
+                value: 30
+            }, {
+                name: 'Romania',
+                value: 10
+            }, {
+                name: 'Germany',
+                value: 2
+            }, {
+                name: 'Iraq',
+                value: 25
+            }]
+        },
+        {
+            name: 'tick borne disease',
+            data: [{
+                name: 'Germany',
+                value: 4
+            }, {
+                name: 'France',
+                value: 6
+            }, {
+                name: 'Chad',
+                value: 3
+            }, {
+                name: 'South Africa',
+                value: 5
+            }, {
+                name: 'Mali',
+                value: 5
+            }]
+        },
+        {
+            name: 'common cold',
+            data: [{
+                name: 'China',
+                value: 2
+            }, {
+                name: 'Iran',
+                value: 3
+            }, {
+                name: 'Spain',
+                value: 8
+            }]
+        },
+    ],
+    "1 month": [
+        {
+            name: 'Zika',
+            data: [{
+                name: 'Germany',
+                value: 20
+            }, {
+                name: 'Croatia',
+                value: 41
+            }, {
+                name: 'Russia',
+                value: 35
+            },{
+                name: 'Egypt',
+                value: 21
+            }, {
+                name: 'Angola',
+                value: 100
+            }, {
+                name: 'Libya',
+                value: 10
+            },{
+                name: 'Turkey',
+                value: 27
+            }, {
+                name: 'Nigeria',
+                value: 53
+            }, {
+                name: 'Congo',
+                value: 93
+            },{
+                name: 'Zambia',
+                value: 28
+            }, {
+                name: 'Kenya',
+                value: 35
+            }, {
+                name: 'Uganda',
+                value: 12
+            }]
+        },
+        {
+            name: 'Ebola',
+            data: [{
+                name: 'Germany',
+                value: 17
+            }, {
+                name: 'Croatia',
+                value: 29
+            }, {
+                name: 'Russia',
+                value: 60
+            },{
+                name: 'Egypt',
+                value: 19
+            }, {
+                name: 'Angola',
+                value: 140
+            }, {
+                name: 'Libya',
+                value: 60
+            },{
+                name: 'Turkey',
+                value: 27
+            }, {
+                name: 'Nigeria',
+                value: 142
+            }]
+        },
+        {
+            name: 'common cold',
+            data: [{
+                name: 'France',
+                value: 50
+            }, {
+                name: 'Denmark',
+                value: 23
+            }, {
+                name: 'Poland',
+                value: 32
+            },{
+                name: 'Egypt',
+                value: 259
+            }, {
+                name: 'Angola',
+                value: 210
+            }, {
+                name: 'Libya',
+                value: 250
+            },{
+                name: 'Turkey',
+                value: 45
+            }, {
+                name: 'Nigeria',
+                value: 120
+            }, {
+                name: 'Congo',
+                value: 350
+            },{
+                name: 'Zambia',
+                value: 70
+            }, {
+                name: 'Kenya',
+                value: 98
+            }, {
+                name: 'China',
+                value: 53
+            }, {
+                name: 'Chile',
+                value: 23
+            },{
+                name: 'Brazil',
+                value: 28
+            }, {
+                name: 'Laos',
+                value: 33
+            }, {
+                name: 'Uganda',
+                value: 12
+            }]
+        },
+        {
+            name: 'pneumonia',
+            data: [{
+                name: 'Germany',
+                value: 20
+            }, {
+                name: 'Croatia',
+                value: 41
+            }, {
+                name: 'Russia',
+                value: 35
+            },{
+                name: 'Egypt',
+                value: 21
+            }, {
+                name: 'Angola',
+                value: 100
+            }, {
+                name: 'Libya',
+                value: 10
+            },{
+                name: 'Turkey',
+                value: 27
+            }, {
+                name: 'Nigeria',
+                value: 53
+            }, {
+                name: 'Congo',
+                value: 93
+            },{
+                name: 'Zambia',
+                value: 28
+            }, {
+                name: 'Kenya',
+                value: 35
+            }, {
+                name: 'Uganda',
+                value: 12
+            }]
+        },
+        {
+            name: 'tick borne disease',
+            data: [{
+                name: 'Germany',
+                value: 48
+            }, {
+                name: 'Croatia',
+                value: 36
+            }, {
+                name: 'Russia',
+                value: 12
+            },{
+                name: 'Egypt',
+                value: 58
+            }, {
+                name: 'Angola',
+                value: 26
+            }, {
+                name: 'Libya',
+                value: 10
+            },{
+                name: 'Turkey',
+                value: 23
+            }, {
+                name: 'Nigeria',
+                value: 53
+            }, {
+                name: 'Congo',
+                value: 93
+            },{
+                name: 'Zambia',
+                value: 43
+            }, {
+                name: 'Kenya',
+                value: 35
+            }, {
+                name: 'Uganda',
+                value: 98
+            }]
+        },
+        {
+            name: 'mosquito borne disease',
+            data: [{
+                name: 'Germany',
+                value: 20
+            }, {
+                name: 'Croatia',
+                value: 41
+            }, {
+                name: 'Russia',
+                value: 35
+            },{
+                name: 'Egypt',
+                value: 21
+            }, {
+                name: 'Angola',
+                value: 100
+            }, {
+                name: 'Libya',
+                value: 10
+            },{
+                name: 'Turkey',
+                value: 27
+            }, {
+                name: 'Nigeria',
+                value: 53
+            }, {
+                name: 'Congo',
+                value: 93
+            },{
+                name: 'Zambia',
+                value: 28
+            }, {
+                name: 'Kenya',
+                value: 35
+            }, {
+                name: 'Uganda',
+                value: 12
+            }]
+        },
+        {
+            name: 'natural disaster',
+            data: [{
+                name: 'Germany',
+                value: 20
+            }, {
+                name: 'Croatia',
+                value: 41
+            }, {
+                name: 'Russia',
+                value: 35
+            },{
+                name: 'Egypt',
+                value: 21
+            }, {
+                name: 'Angola',
+                value: 100
+            }, {
+                name: 'Libya',
+                value: 10
+            },{
+                name: 'Turkey',
+                value: 27
+            }, {
+                name: 'Nigeria',
+                value: 53
+            }, {
+                name: 'Congo',
+                value: 93
+            },{
+                name: 'Zambia',
+                value: 28
+            }, {
+                name: 'Kenya',
+                value: 35
+            }, {
+                name: 'Uganda',
+                value: 12
+            }]
+        },
+    ],
+}
+var selectedPeriod = 'Now';
+var categories = [];
+
+function displayHighcharts(data) {
+    Highcharts.chart('bubble-chart', {
+        chart: {
+            type: 'packedbubble',
+            height: '100%',
+        },
+        title: {
+            text: 'bubble chart'
+        },
+        legend: {
+            align: 'right',
+            verticalAlign: 'top',
+            layout: 'vertical',
+            x: 0,
+            y: 150,
+            fontSize: 16,
+      },
+        tooltip: {
+            useHTML: true,
+            pointFormat: '<b>{point.name}:</b> {point.value}'
+        },
+        plotOptions: {
+            packedbubble: {
+                minSize: '20%',
+                maxSize: '100%',
+                zMin: 0,
+                zMax: 1000,
+                layoutAlgorithm: {
+                    gravitationalConstant: 0.05,
+                    splitSeries: true,
+                    seriesInteraction: false,
+                    dragBetweenSeries: true,
+                    parentNodeLimit: true
+                },
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}',
+                    filter: {
+                        property: 'y',
+                        operator: '>',
+                        value: 50
+                    },
+                    style: {
+                        color: 'black',
+                        textOutline: 'none',
+                        fontWeight: 'normal'
+                    }
+                }
+            }
+        },
+        series: data,
+    });
+}
+
+function controlChart(selectedPeriod) {
+    displayHighcharts(data[selectedPeriod]);
+}
+controlChart(selectedPeriod);
+
+$('li.period-item').on('click', function(e) {
+    e.preventDefault();
+    categories = [];
+    $('li.period-item').removeClass('active');
+    $(this).addClass('active');
+    var period = $(this).html();
+    console.log('period ===> ', period);
+    controlChart(period);
+})
+
+$('li.category-item').on('click', function(e) {
+    e.preventDefault();
+    $('li.category-item').removeClass('active');
+    $(this).addClass('active');
+
+})
